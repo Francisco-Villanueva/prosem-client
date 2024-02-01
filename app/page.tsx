21528da665996d153d08dd2ae3f9180695d4bc39
Marianoromero8 <marianoromero97.mr@gmail.com>
2024-02-01 17:16:04 -0300

    fixed: imports are deleted

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
 import LandingPage from "@/app/components/LandingPage";
-import Card from "./common/Card";
-import { ClickIcon, CustomIcon, SecureIcon, StaffIcon } from "./icons";
-import FeatureCards from "./components/FeatureCards";
-
 
 export default function Home() {
   return (
