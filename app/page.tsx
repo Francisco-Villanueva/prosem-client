8e0a67b1a6d14dc6584c959d953b736d84209d94
Marianoromero8 <marianoromero97.mr@gmail.com>
2024-02-01 11:26:14 -0300

    fix(clean): deleted trash from page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Button from "@/app/common/Button";
 import LandingPage from "@/app/components/LandingPage";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center gap-2 h-[100vh] ">
-      {/* <LandingPage /> */}
-      <Button variant="primary">BUTTON</Button>
-      <Button variant="secondary">button</Button>
-      <Button variant="dark">button</Button>
+      <LandingPage />
     </main>
   );
 }
