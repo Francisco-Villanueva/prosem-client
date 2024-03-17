a69c3186656aea449cf5535c2b1b2b37b9d5e236
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-03-17 13:07:08 -0300

    fixed ui pricing cards

--- app/components/PricingPage.tsx
+++ app/components/PricingPage.tsx
@@ -1,43 +1,6 @@
 import React from "react";
 import PageLayout from "../common/PageLayout";
-import Button from "../common/Button";
 import { PricingCard } from ".";
-function CheckIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-6 h-6 text-green"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-      />
-    </svg>
-  );
-}
-function XIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-6 h-6 text-error"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-      />
-    </svg>
-  );
-}
 
 export function PricingPage() {
   return (
